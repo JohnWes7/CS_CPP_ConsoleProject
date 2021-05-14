705829126dfd5db08622de18dbb437a8e5f72bfb
JohnWes7 <1246215651@qq.com>
2021-05-15 01:46:29 +0800

    tset

---
+++ 001/Test_Field/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Field
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Student
+    {
+        string major;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string major) : this()
+        {
+            
+        }
+    }
+}
